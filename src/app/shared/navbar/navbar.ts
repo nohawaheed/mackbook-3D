588672e7fde797f6d34dccdc6998c18d92347dc3
nohawaheed <nohawaheed.1991@gmail.com>
2026-01-04 23:35:52 +0200

    feat: add navbar component

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+
+  navLinks = [
+    { label: "Store" },
+    { label: "Mac" },
+    { label: "iPhone" },
+    { label: "Watch" },
+    { label: "Vision" },
+    { label: "Airpods" },
+  ]
+
+}
