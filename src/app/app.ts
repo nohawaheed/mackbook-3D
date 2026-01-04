7ac6fdf05b4106c0d0a16cb7bd4273052bf99458
nohawaheed <nohawaheed.1991@gmail.com>
2026-01-05 00:06:58 +0200

    feat: update app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./shared/navbar/navbar";
+import { Hero } from "./features/hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
